f20b775425432fc2519bf963295c011d05e710d2
Oleg <kurtickijoleg@gmail.com>
2025-05-14 13:36:57 +0300

    app

---
+++ app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Product, Category, Brand
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Brand)
+# Register your models here.
