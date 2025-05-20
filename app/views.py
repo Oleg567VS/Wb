8e0805a00291d80c9a77fe5de25de6babc4baf8c
Oleg <kurtickijoleg@gmail.com>
2025-05-20 18:37:10 +0300

    New

--- app/views.py
+++ app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 
-
 def index(request):
-    products = Product.objects.all()
+    # Получаем все продукты
+    products_list = Product.objects.all()
+    
+    # Создаем пагинатор - 12 товаров на страницу
+    paginator = Paginator(products_list, 12)
+    
+    # Получаем номер страницы из GET-параметра
+    page = request.GET.get('page')
+    
+    try:
+        # Получаем объекты для запрошенной страницы
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        # Если page не целое число, показываем первую страницу
+        products = paginator.page(1)
+    except EmptyPage:
+        # Если page вне диапазона (например, 9999), показываем последнюю страницу
+        products = paginator.page(paginator.num_pages)
+    
     context = {"products": products}
     return render(request, "app/index.html", context)
-
 
 def product_details(request, product_id):
     product = Product.objects.get(id=product_id)
